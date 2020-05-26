037972b73c88ecfe6b42e5b293cb3f5f825d1d93
Oliver Radini <oliver.radini@wheelright.co.uk>
2020-05-26 11:01:13 +0100

    finished homework 2

--- CIS_194_002/LogAnalysis.hs
+++ CIS_194_002/LogAnalysis.hs
@@ -15,6 +15,34 @@ separatedWordsToLogMessage s                                = Unknown (unwords s
 parseMessage :: String -> LogMessage
 parseMessage = separatedWordsToLogMessage . words
 
-
 parse :: String -> [LogMessage]
 parse = (map parseMessage) . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert lm@LogMessage{} Leaf = Node Leaf lm Leaf
+insert newLogMessage@(LogMessage _ newTimestamp _) (Node left rootNode@(LogMessage _ rootTimestamp _) right)
+    | newTimestamp > rootTimestamp = (Node left rootNode (insert newLogMessage right))
+    | otherwise = (Node (insert newLogMessage left) rootNode right)
+insert _ tree = tree
+
+build :: [LogMessage] -> MessageTree
+build ms = (foldr insert Leaf ms)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf         = []
+inOrder (Node l m r) = (inOrder l) ++ [m] ++ (inOrder r)
+
+hasSeverityGreaterThan :: Int -> LogMessage -> Bool
+hasSeverityGreaterThan requiredSeverity (LogMessage (Error s) _ _) = s > requiredSeverity
+hasSeverityGreaterThan _ _                        = False
+
+takeLogMessage :: LogMessage -> String
+takeLogMessage (LogMessage _ _ m) = m
+takeLogMessage _                  = ""
+
+takeSeverity :: LogMessage -> Int
+takeSeverity (LogMessage (Error s) _ _) = s
+takeSeverity                          _ = 0
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = (map takeLogMessage) . inOrder . build . (filter (hasSeverityGreaterThan 49))
