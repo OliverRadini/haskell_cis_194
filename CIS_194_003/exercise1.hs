29fe6118a3c41d8eab39b082ee095370ce861eb9
Oliver Radini <oliver.radini@wheelright.co.uk>
2020-05-31 13:14:06 +0100

    first pass at exercise 2

--- CIS_194_003/exercise1.hs
+++ CIS_194_003/exercise1.hs
@@ -4,5 +4,9 @@ filterWithIndex f x = map (\(_, x) -> x) (filter f (zip [a+1|a<-[0..]] x))
 skips :: [a] -> [[a]]
 skips x = [(filterWithIndex (\(i, _) -> (i `mod` (y + 1) == 0)) x) | y <- [0..(length x)]]
 
+localMaxima :: [Integer] -> [Integer]
+localMaxima (a : b : c : xs) = (if a < b && b > c then [b] else []) ++ (localMaxima ([c] ++ xs))
+localMaxima x                = []
+
 main = do
   print (skips [True, False])
